38f581ac363b3b467d66b3387adff90738f8c360
Nicholas Breen <nick@breen.kiwi>
2024-01-31 20:57:46 +1300

    test test fixures generated by bazel

--- src/test/java/nickbreen.bes/FixturesTest.java
+++ src/test/java/nickbreen.bes/FixturesTest.java
@@ -8,16 +8,16 @@ import javax.json.JsonObject;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.hamcrest.CoreMatchers.everyItem;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
-import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertTrue;
 
 public class FixturesTest
 {
-    private final String expectedId = "f6a5e727-0032-4a84-a959-61e4ecb294e9";
+    private final String expectedUuid = "f6a5e727-0032-4a84-a959-61e4ecb294e9";
 
     @Test
     public void shouldReadAllJsonEventsAsJsonObjects()
@@ -26,19 +26,35 @@ public class FixturesTest
     }
 
     @Test
-    public void shouldIdentifyBuildInvocationId() throws IOException
+    public void shouldReadAllBinaryEventsAsBazelBuildEvents() throws IOException
     {
-        final InputStream bes = FixturesTest.class.getResourceAsStream("/bes.bin");
-
-        final ArrayList<BuildEventStreamProtos.BuildEvent> events = new ArrayList<>();
-
-        for (BuildEventStreamProtos.BuildEvent message = BuildEventStreamProtos.BuildEvent.parseDelimitedFrom(bes); null != message; message = BuildEventStreamProtos.BuildEvent.parseDelimitedFrom(bes))
-        {
-            events.add(message);
-        }
+        final List<BuildEventStreamProtos.BuildEvent> events = loadBinaryBazelBuildEvents();
 
         assertThat(events, hasSize(33));
         assertThat(events, everyItem(notNullValue()));
+    }
+
+    @Test
+    public void shouldIdentifyInvocationFromFirstBuildEvent() throws IOException
+    {
+        final List<BuildEventStreamProtos.BuildEvent> events = loadBinaryBazelBuildEvents();
+
+        final BuildEventStreamProtos.BuildEvent first = events.get(0);
+        assertTrue("hasStarted", first.hasStarted());
+        assertThat(first.getStarted().getUuid(), is(expectedUuid));
+    }
+
+    private static List<BuildEventStreamProtos.BuildEvent> loadBinaryBazelBuildEvents() throws IOException
+    {
+        final List<BuildEventStreamProtos.BuildEvent> events = new ArrayList<>();
+        try (final InputStream bes = FixturesTest.class.getResourceAsStream("/bes.bin"))
+        {
+            for (BuildEventStreamProtos.BuildEvent message = BuildEventStreamProtos.BuildEvent.parseDelimitedFrom(bes); null != message; message = BuildEventStreamProtos.BuildEvent.parseDelimitedFrom(bes))
+            {
+                events.add(message);
+            }
+        }
+        return events;
     }
 
 
