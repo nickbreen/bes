3e6f69d857377e99e74e7dc98c644e783cc9ea7c
Nicholas Breen <nick@breen.kiwi>
2024-02-08 22:20:26 +1300

    fix test after changing behaviour

--- src/test/java/nickbreen.bes/processor/BazelBuildEventProcessorTest.java
+++ src/test/java/nickbreen.bes/processor/BazelBuildEventProcessorTest.java
@@ -6,23 +6,16 @@ import com.google.devtools.build.v1.OrderedBuildEvent;
 import com.google.devtools.build.v1.StreamId;
 import com.google.protobuf.Any;
 import com.google.protobuf.Message;
-import nickbreen.bes.AnyMatcher;
-import nickbreen.bes.processor.BazelBuildEventProcessor;
-import nickbreen.bes.processor.BuildEventProcessor;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-import static nickbreen.bes.AnyMatcher.anyThat;
 import static nickbreen.bes.MessageMatcher.messageThat;
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.notNullValue;
 
 public class BazelBuildEventProcessorTest
 {
@@ -32,24 +25,23 @@ public class BazelBuildEventProcessorTest
         final String uuid = UUID.randomUUID().toString();
         final List<Message> actual = new ArrayList<>();
         final BuildEventProcessor processor = new BazelBuildEventProcessor(actual::add);
-        final OrderedBuildEvent actualEvent = OrderedBuildEvent.newBuilder()
+        processor.accept(OrderedBuildEvent.newBuilder()
                 .setStreamId(StreamId.newBuilder().setInvocationId(uuid).setComponent(StreamId.BuildComponent.CONTROLLER))
                 .setSequenceNumber(1)
                 .setEvent(BuildEvent.newBuilder()
                         .setBazelEvent(Any.pack(BuildEventStreamProtos.BuildEvent.newBuilder()
                                 .setStarted(BuildEventStreamProtos.BuildStarted.newBuilder().setUuid(uuid))
                                 .build())))
-                .build();
-        processor.accept(actualEvent);
-        assertThat("has stream ID for UUID", actual, hasItem(
-                messageThat(OrderedBuildEvent.class, OrderedBuildEvent::hasStreamId, OrderedBuildEvent::getStreamId,
-                        messageThat(StreamId.class, s -> StreamId.BuildComponent.CONTROLLER.equals(s.getComponent()), StreamId::getInvocationId, is(uuid)))));
+                .build());
+        processor.accept(OrderedBuildEvent.newBuilder()
+                .setStreamId(StreamId.newBuilder().setInvocationId(uuid).setComponent(StreamId.BuildComponent.CONTROLLER))
+                .setSequenceNumber(1)
+                .setEvent(BuildEvent.newBuilder()
+                        .setBazelEvent(Any.pack(BuildEventStreamProtos.BuildEvent.newBuilder()
+                                .setFinished(BuildEventStreamProtos.BuildFinished.newBuilder().setExitCode(BuildEventStreamProtos.BuildFinished.ExitCode.newBuilder().setCode(0)))
+                                .build())))
+                .build());
 
-        assertThat("has bazel build started event for UUID", actual, hasItem(
-                messageThat(OrderedBuildEvent.class, OrderedBuildEvent::hasEvent, OrderedBuildEvent::getEvent,
-                        anyThat(BuildEvent.class, BuildEventStreamProtos.BuildEvent.class, BuildEvent::hasBazelEvent, BuildEvent::getBazelEvent,
-                                messageThat(BuildEventStreamProtos.BuildEvent.class, BuildEventStreamProtos.BuildEvent::hasStarted, BuildEventStreamProtos.BuildEvent::getStarted,
-                                        messageThat(BuildEventStreamProtos.BuildStarted.class, BuildEventStreamProtos.BuildStarted::getUuid,
-                                                is(uuid)))))));
+        assertThat("has bazel build started event for UUID", actual, hasItem(messageThat(BuildEventStreamProtos.BuildStarted.class, BuildEventStreamProtos.BuildStarted::getUuid, is(uuid))));
     }
 }
